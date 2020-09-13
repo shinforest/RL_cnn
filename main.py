8b91717416b5c9d4fa3b41d44d265bd2cd57c314
okamurashin <threetrees1355@gmail.com>
2020-09-13 13:26:34 +0900

    we should modify state form and in future I try convolutional

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ import os
 import shutil
 import random
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 from collections import deque
 from image_env import IMAGE
 from DQN_Net import Policy
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
+from load_model import Model
+m = Model()
+model = m.load_model()
 
 
 grid=IMAGE()
-policy=Policy(12,4)
+policy=Policy(7500,7)
 t_action=grid.t_action
 r_log=[]
 for episode in range(1000):
@@ -24,14 +27,20 @@ for episode in range(1000):
     state=grid.state
     done=grid.done
     actions=[]
+    print(episode)
     while not done:
         action=policy.choose_action(state)
-        observation=grid.step(action,episode)
-        n_state=observation[0]
+        observation=grid.step(action,episode,model)
+        n_state=observation[0].reshape(-1)
         reward=observation[1]
         done=observation[2]
+        print(reward)
+        print(state.shape)
+        print(n_state.shape)
         policy.learn_act(state,reward,n_state)
         state=n_state
         r_log.append(reward)
         actions.append(t_action[action])
     print(episode,actions)
+
+np.save("r_log", r_log)
---
+++ np_test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr=np.load(
+    file="test_np.npy")
+print(arr)
